5f83ccf7b95e742ad9a73442f30e8bded06db999
sebaslosorio <70674182+sebaslosorio@users.noreply.github.com>
2021-05-19 18:33:49 -0500

    Finished Tab Navigation

---
+++ navigations/AccountStack.js
@@ -0,0 +1,17 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import Account from '../screens/Account'
+
+const Stack = createStackNavigator()
+
+export default function AccountStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="account"
+                component={Account}
+                options={{title: "Cuenta"}}
+            />
+        </Stack.Navigator>
+    )
+}
