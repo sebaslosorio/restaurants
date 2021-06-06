fba4844ed2d0829077ba60cc29e5202141794fb2
sebaslosorio <70674182+sebaslosorio@users.noreply.github.com>
2021-06-06 17:34:09 -0500

    create map and show view

--- components/restaurants/AddRestaurantForm.js
+++ components/restaurants/AddRestaurantForm.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
-import { StyleSheet, View, ScrollView, Alert } from 'react-native'
-import { Input, Button, Icon, Avatar } from 'react-native-elements'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, ScrollView, Alert, Dimensions, Text } from 'react-native'
+import { Input, Button, Icon, Avatar, Image } from 'react-native-elements'
 import CountryPicker from 'react-native-country-picker-modal'
 import { map, size, filter } from 'lodash'
-import { loadImageFromGallery } from '../../utils/helpers'
+import MapView from 'react-native-maps'
+import { getCurrentLocation, loadImageFromGallery } from '../../utils/helpers'
+import Modal from '../../components/Modal'
+
+const widthScreen = Dimensions.get("window").width
 
 export default function AddRestaurantForm({ toastRef, setLoading, navigation }) {
     const [formData, setFormData] = useState(defaultFormValues())
@@ -13,6 +17,10 @@ export default function AddRestaurantForm({ toastRef, setLoading, navigation }) 
     const [errorPhone , setErrorPhone] = useState(null)
     const [errorAddress , setErrorAddress] = useState(null)
     const [imagesSelected, setImagesSelected] = useState([])
+    const [isVisibleMap, setIsVisibleMap] = useState(false)
+    const [locationRestaurant, setLocationRestaurant] = useState(null)
+      
+
 
     const addRestaurant = () => {
         console.log(formData)
@@ -20,7 +28,10 @@ export default function AddRestaurantForm({ toastRef, setLoading, navigation }) 
     }
 
     return (
-        <View style={styles.viewContainer}>
+        <ScrollView style={styles.viewContainer}>
+            <ImageRestaurant
+                imageRestaurant = {imagesSelected[0]}
+            />
             <FormAdd
                 formData={formData}
                 setFormData={setFormData}
@@ -29,6 +40,7 @@ export default function AddRestaurantForm({ toastRef, setLoading, navigation }) 
                 errorEmail={errorEmail}
                 errorPhone={errorPhone}
                 errorAddress={errorAddress}
+                setIsVisibleMap={setIsVisibleMap}
             />
             <UploadImage
                 toastRef={toastRef}
@@ -40,81 +52,87 @@ export default function AddRestaurantForm({ toastRef, setLoading, navigation }) 
                 title="Crear Restaurante"
                 onPress={addRestaurant}
             />
-        </View>
+            <MapRestaurant
+                isVisibleMap={isVisibleMap}
+                setIsVisibleMap={setIsVisibleMap}
+                locationRestaurant={locationRestaurant}
+                setLocationRestaurant={setLocationRestaurant}
+                toastRef={toastRef}
+            />
+        </ScrollView>
     )
 }
 
-const defaultFormValues = () => {
-    return {
-        name : "",
-        description : "",
-        email : "",
-        phone : "",
-        address : "",
-        country : "CO",
-        callingCode : "57"
-    }
-}
+function MapRestaurant({ isVisibleMap, setIsVisibleMap, setLocationRestaurant, locationRestaurant, toastRef }) {
 
-function FormAdd({ formData, setFormData, errorName, errorDescription, errorEmail, errorPhone, errorAddress}) {
-    const [country, setCountry] = useState("CO")
-    const [callingCode, setCallingCode] = useState("57")
-    const [phone, setPhone] = useState("")
-    const onChange = (e, type) => {
-        setFormData({...formData, [type] : e.nativeEvent.text })
+    const [newRegion, setNewRegion] = useState(null)
+
+    useEffect(() => {
+    (async()=>{
+        const response = await getCurrentLocation()
+            if (response.status) {
+                setLocationRestaurant(response.location)
+            }
+    })()
+    }, [])
+
+    const confirmLocation = () => {
+        setLocationRestaurant(newRegion)
+        toastRef.current.show("Localizacion guardada correctamente.", 3000)
+        setIsVisibleMap(false)
+        console.log(newRegion)
     }
 
     return (
-        <View style={styles.viewForm}>
-            <Input
-                placeholder="Nombre del restaurante..."
-                defaultFormValues={formData.name}
-                onChange={(e) => onChange(e, "name")}
-                errorMessage={errorName}
-            />
-            <Input
-                placeholder="Direccion del restaurante..."
-                defaultFormValues={formData.address}
-                onChange={(e) => onChange(e, "address")}
-                errorMessage={errorAddress}
-            />
-            <Input                    
-                keyboardType="email-address"
-                placeholder="Email del restaurante..."
-                defaultFormValues={formData.email}
-                onChange={(e) => onChange(e, "email")}
-                errorMessage={errorEmail}
-            />
-            <View style={styles.phoneView}>
-                <CountryPicker
-                    withFlag
-                    withCallingCode
-                    withFilter
-                    withCallingCodeButton
-                    containerStyle={styles.countryPicker}
-                    countryCode={country}
-                    onSelect={(country) => {
-                        setFormData({...formData, "country" : country.cca2, "callingCode" : callingCode[0]})
-                    }}
-                />
-                <Input
-                    containerStyle={styles.inputPhone}
-                    defaultFormValues={formData.phone}
-                    onChange={(e) => onChange(e, "phone")}
-                    errorMessage={errorPhone}
-                    keyboardType="phone-pad"
-                    placeholder="WhatsApp del Restaurante..."
-
-                />
+        <Modal isVisible={isVisibleMap} setVisible={setIsVisibleMap}>
+            <View>
+                {
+                    locationRestaurant && (
+                        <MapView
+                            style={styles.mapStyle}
+                            initialRegion={locationRestaurant}
+                            showsUserLocation
+                            onRegionChange={(region) => setNewRegion(region)}
+                        >
+                            <MapView.Marker
+                                coordinate={{
+                                    latitude : locationRestaurant.latitude,
+                                    longitude : locationRestaurant.longitude
+                                }}
+                                draggable
+                            />
+                        </MapView>
+                    )
+                }
+                <View style={styles.viewMapBtn}>
+                    <Button
+                        title="Guardar"
+                        containerStyle={styles.viewMapContainerSave}
+                        buttonStyle={styles.viewMapBtnSave}
+                        onPress={confirmLocation}
+                    />
+                    <Button
+                        title="Cancelar"
+                        containerStyle={styles.viewMapContainerCancel}
+                        buttonStyle={styles.viewMapBtnCancel}
+                        onPress={()=>setIsVisibleMap(false)}
+                    />
+                </View>
             </View>
-            <Input
-                    containerStyle={styles.textArea}
-                    defaultFormValues={formData.description}
-                    onChange={(e) => onChange(e, "description")}
-                    errorMessage={errorDescription}
-                    multiline
-                    placeholder="Descripcion del Restaurante..."
-                />
+
+        </Modal>        
+    )
+}
+
+function ImageRestaurant ({imageRestaurant}) {
+    return (
+        <View style={styles.viewPhoto}>
+            <Image
+                source={
+                    imageRestaurant ? { uri : imageRestaurant } : require("../../assets/no-image.png")
+                }
+                style={{width : widthScreen, height : 200}}
+            />
         </View>
     )
 }
@@ -183,6 +201,87 @@ function UploadImage ({toastRef, imagesSelected, setImagesSelected}) {
     )
 }
 
+function FormAdd({ formData, setFormData, errorName, errorDescription, errorEmail, errorPhone, errorAddress, setIsVisibleMap}) {
+    const [country, setCountry] = useState("CO")
+    const [callingCode, setCallingCode] = useState("57")
+    const [phone, setPhone] = useState("")
+    const onChange = (e, type) => {
+        setFormData({...formData, [type] : e.nativeEvent.text })
+    }
+
+    return (
+        <View style={styles.viewForm}>
+            <Input
+                placeholder="Nombre del restaurante..."
+                defaultFormValues={formData.name}
+                onChange={(e) => onChange(e, "name")}
+                errorMessage={errorName}
+            />
+            <Input
+                placeholder="Direccion del restaurante..."
+                defaultFormValues={formData.address}
+                onChange={(e) => onChange(e, "address")}
+                rightIcon={{
+                    type:"material-community",
+                    name:"google-maps",
+                    color:"#c2c2c2",
+                    onPress:() => setIsVisibleMap(true)
+                }}
+                errorMessage={errorAddress}
+            />
+            <Input                    
+                keyboardType="email-address"
+                placeholder="Email del restaurante..."
+                defaultFormValues={formData.email}
+                onChange={(e) => onChange(e, "email")}
+                errorMessage={errorEmail}
+            />
+            <View style={styles.phoneView}>
+                <CountryPicker
+                    withFlag
+                    withCallingCode
+                    withFilter
+                    withCallingCodeButton
+                    containerStyle={styles.countryPicker}
+                    countryCode={country}
+                    onSelect={(country) => {
+                        setFormData({...formData, "country" : country.cca2, "callingCode" : callingCode[0]})
+                    }}
+                />
+                <Input
+                    containerStyle={styles.inputPhone}
+                    defaultFormValues={formData.phone}
+                    onChange={(e) => onChange(e, "phone")}
+                    errorMessage={errorPhone}
+                    keyboardType="phone-pad"
+                    placeholder="WhatsApp del Restaurante..."
+
+                />
+            </View>
+            <Input
+                containerStyle={styles.textArea}
+                defaultFormValues={formData.description}
+                onChange={(e) => onChange(e, "description")}
+                errorMessage={errorDescription}
+                multiline
+                placeholder="Descripcion del Restaurante..."
+            />
+        </View>
+    )
+}
+
+const defaultFormValues = () => {
+    return {
+        name : "",
+        description : "",
+        email : "",
+        phone : "",
+        address : "",
+        country : "CO",
+        callingCode : "57"
+    }
+}
+
 const styles = StyleSheet.create({
     viewContainer : {
         height : "100%",
@@ -226,5 +325,31 @@ const styles = StyleSheet.create({
         width : 70,
         height : 70,
         marginRight : 10
+    }, 
+    viewPhoto : {
+        alignItems : "center",
+        height : 200,
+        marginBottom : 20
+    }, 
+    mapStyle : {
+        width : "100%",
+        height : 550
+    }, 
+    viewMapBtn : {
+        flexDirection : "row",
+        justifyContent : "center",
+        marginTop : 10
+    }, 
+    viewMapContainerCancel : {
+        paddingLeft : 5
+    },
+    viewMapContainerSave : {
+        paddingRight : 5
+    },
+    viewMapBtnCancel : {
+        backgroundColor : "#a65273"
+    }, 
+    viewMapBtnSave : {
+        backgroundColor : "#442484"
     }
 })
